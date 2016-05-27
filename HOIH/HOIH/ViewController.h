bb93a75c63accfd35e611ef079937f5b2faf5157
ZHONG Lucheng <zhonglucheng@gmail.com>
2016-05-27 15:56:15 +0200

    init push

---
+++ HOIH/HOIH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HOIH
+//
+//  Created by 钟路成 on 16/5/27.
+//  Copyright © 2016年 Lucheng Zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HOIH/HOIH/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HOIH
+//
+//  Created by 钟路成 on 16/5/27.
+//  Copyright © 2016年 Lucheng Zhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
